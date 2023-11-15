fea4baf43bbcccee517cdd8b3c84ac0a4c34153c
Anthony <aquinoanthony851@gmail.com>
2023-11-15 09:22:48 -0700

    Routes Lab

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,18 @@
+import NavBar from "../components/NavBar";
+import { useRouterError } from "react-router-dom"
+
+function ErrorPage() {
+  const error = useRouterError();
+  console.error(error);
+  
+  return ( 
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <h1>Oops! Looks like something went wrong.</h1>
+    </>
+   );
+}
+
+export default ErrorPage;
